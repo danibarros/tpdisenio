7e9790fa7f966f3d2027c6865a184470b2f1cda9
corteseemiliano@gmail.com <corteseemiliano@gmail.com@b5a30a6b-5324-f511-f870-38aa0189b1d0>
2013-08-13 23:41:33 +0000

    Sos gay

--- trunk/PPADiseño/src/edu/ventas/entities/Butaca.java
+++ trunk/PPADiseño/src/edu/ventas/entities/Butaca.java
@@ -3,10 +3,18 @@ package edu.ventas.entities;
 public class Butaca {
 	
 	private int numero;
-
+	private Fila fila;
+	
 	public Butaca(int numero) {
-		
 		this.numero = numero;
+	}
+
+	public Fila getFila() {
+		return fila;
+	}
+
+	public void setFila(Fila fila) {
+		this.fila = fila;
 	}
 
 	public int getNumero() {
