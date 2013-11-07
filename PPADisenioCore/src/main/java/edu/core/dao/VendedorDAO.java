63d7c35ab916796d7d0e898acb99ea738f1db6f7
dani.barros18@gmail.com <dani.barros18@gmail.com@b5a30a6b-5324-f511-f870-38aa0189b1d0>
2013-11-07 00:25:10 +0000

    ya va queriendo VENTAS.. esta mejorada un poquito la funcionalidad y lo que va haciendo..

--- PPADisenioCore/src/main/java/edu/core/dao/VendedorDAO.java
+++ PPADisenioCore/src/main/java/edu/core/dao/VendedorDAO.java
@@ -2,7 +2,6 @@ package edu.core.dao;
 
 import org.hibernate.Query;
 
-import edu.core.entities.Banda;
 import edu.core.entities.Estadio;
 import edu.core.entities.Vendedor;
 
@@ -13,6 +12,18 @@ public class VendedorDAO extends GenericRepository<Estadio, Integer>{
 		String hql = "from Vendedor where vendedor_user = :vendedorNombre ";
 		Query query = session.createQuery(hql);
 		query.setString("vendedorNombre", user);
+		Vendedor result = null;
+		if(!query.list().isEmpty()){
+			result = (Vendedor) query.list().get(0);
+		}
+		return result;
+	}
+	
+	public Vendedor getVendedarDatosById(Integer id)
+	{
+		String hql="from Vendedor where vendedor_id = :id";
+		Query query = session.createQuery(hql);
+		query.setString("id",id.toString());
 		Vendedor result = null;
 		if(!query.list().isEmpty()){
 			result = (Vendedor) query.list().get(0);
